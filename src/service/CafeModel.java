d4e3aac6c80c950002d4fea5d329ce749e8f0854
mandariny <mandariny716@gmail.com>
2022-01-17 18:12:30 +0900

    Add Model logig

--- src/service/CafeModel.java
+++ src/service/CafeModel.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import exception.IdDuplicationException;
 import exception.IdNotFoundException;
 import exception.NotEnoughInfoException;
+import exception.NotEnoughMoneyException;
+import exception.ReservationDuplicationException;
 import model.dto.CafeUser;
 
 public class CafeModel {
@@ -38,7 +40,7 @@ public class CafeModel {
 				user.setPnum(pnum);
 			}
 		}
-		throw new IdNotFoundException("아이디가 없습니다.");
+		throw new IdNotFoundException("아이디를 찾을 수 없습니다.");
 	}
 	
 	public int deleteUserInfo(String id) throws IdNotFoundException {	
@@ -47,7 +49,7 @@ public class CafeModel {
 				return user.getPoint();
 			}
 		}
-		throw new IdNotFoundException("아이디가 없습니다.");
+		throw new IdNotFoundException("아이디를 찾을 수 없습니다.");
 	}
 	
 	public CafeUser searchUserInfo(String id) {
@@ -55,22 +57,62 @@ public class CafeModel {
 	}
 	
 	public ArrayList<CafeUser> getAllUserInfo(){
-		return null;
+		return cafeUserList;
 	}
 	
-	public void reserve(String id, int point, int time) {
+	public void reserve(String id, int point, int time) throws IdNotFoundException, ReservationDuplicationException, NotEnoughMoneyException{
+		for(CafeUser user : cafeUserList) {
+			if(user.getId().equals(id)){
+				if(user.getReserveTime() != 0) {
+					throw new ReservationDuplicationException("이미 예약된 고객입니다.");
+				}
+				if(user.getPoint() < time * 2000) {
+					throw new NotEnoughMoneyException("결제 금액이 부족합니다.");
+				}
+				
+				user.setReserveTime(time);
+				user.setPoint(user.getPoint() - time * 2000);
+			}
+		}
+		throw new IdNotFoundException("아이디를 찾을 수 없습니다.");
+	}
+	
+	public void cancleReservation(String id) throws IdNotFoundException{
+		for(CafeUser user : cafeUserList) {
+			if(user.getId().equals(id)){
+				user.setPoint(user.getReserveTime() * 2000);
+				user.setReserveTime(0);
+			}
+		}
+		throw new IdNotFoundException("아이디를 찾을 수 없습니다.");
+	}
+	
+	public void updateReservation(String id, int time) throws IdNotFoundException, NotEnoughMoneyException{
+		for(CafeUser user : cafeUserList) {
+			if(user.getId().equals(id)){
+				if(user.getReserveTime() < time) {
+					if(user.getPoint() < (time - user.getReserveTime()) * 2000) {
+						throw new NotEnoughMoneyException("추가 결제 금액이 부족합니다.");
+					}else {
+						user.setPoint(user.getPoint() - ((time - user.getReserveTime()) * 2000));
+						user.setReserveTime(time);
+					}
+				}else {
+					user.setPoint(user.getPoint() + ((user.getReserveTime() - time) * 2000));
+					user.setReserveTime(time);
+				}
+			}
+		}
+		throw new IdNotFoundException("아이디를 찾을 수 없습니다.");
 		
 	}
 	
-	public void cancleReservation(String id) {
-		
-	}
-	
-	public void updateReservation(String id, int time) {
-		
-	}
-	
-	public void chargePoint(String id, int money) {
-		
+	public void chargePoint(String id, int money) throws IdNotFoundException {
+		for(CafeUser user : cafeUserList) {
+			if(user.getId().equals(id)){
+				user.setPoint(user.getPoint() + money);
+			}
+		}
+		throw new IdNotFoundException("아이디를 찾을 수 없습니다.");
 	}
 }
